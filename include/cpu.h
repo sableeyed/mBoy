f389a308a07e511b60414e3db919aef65657f111
mel <1906931+sableeyed@users.noreply.github.com>
2025-09-25 16:23:43 -0500

    Update cpu.h

--- include/cpu.h
+++ include/cpu.h
@@ -26,4 +26,32 @@ void cpu_step(cpu_t *cpu);
 
 void cpu_unimplemented(cpu_t *cpu, uint8_t opcode);
 
+//CPU opcodes
+//bcp is shorthand for BC Pointer. We are loading a memory address into the register, not a value
+void nop(); //0x00
+void ld_bc_u16(uint16_t value); //0x01
+void ld_bcp_a(); //0x02
+void inc_bc(); //0x03
+void inc_b(); //0x04
+void dec_b(); //0x05
+void ld_b_u8(uint8_t value); //0x06
+void rlca(); //0x07
+void ld_u16_sp(uint16_t value); //0x08
+void add_hl_bc(); //0x09
+void ld_a_bcp(); //0x0a
+void dec_bc(); //0x0b
+void inc_c(); //0x0c
+void dec_c(); //0x0d
+void ld_c_u8(uint8_t value); //0x0e
+void rrca(); //0x0f
+void stop(); //0x10
+void ld_de_u16(uint16_t value); //0x11
+void ld_dep_a(); //0x12
+void inc_de(); //0x13
+void inc_d(); //0x14
+void dec_d(); //0x15
+void ld_d_u8(uint8_t value); //0x16
+void rla(); //0x17
+void jr_i8(uint8_t value); //0x18
+
 #endif
