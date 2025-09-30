0b01635e82fc8255c1d5a705052be0e3e543cc9b
mel <1906931+sableeyed@users.noreply.github.com>
2025-09-30 11:27:53 -0500

    Update cpu.h

--- include/cpu.h
+++ include/cpu.h
@@ -29,68 +29,68 @@ void cpu_unimplemented(cpu_t *cpu, uint8_t opcode);
 //CPU opcodes
 //bcp is shorthand for BC Pointer. We are loading a memory address into the register, not a value
 void nop(); //0x00
-void ld_bc_u16(uint16_t value); //0x01
+void ld_bc_u16(uint16_t operand); //0x01
 void ld_bcp_a(); //0x02
 void inc_bc(); //0x03
 void inc_b(); //0x04
 void dec_b(); //0x05
-void ld_b_u8(uint8_t value); //0x06
+void ld_b_u8(uint8_t operand); //0x06
 void rlca(); //0x07
-void ld_u16_sp(uint16_t value); //0x08
+void ld_u16_sp(uint16_t operand); //0x08
 void add_hl_bc(); //0x09
 void ld_a_bcp(); //0x0a
 void dec_bc(); //0x0b
 void inc_c(); //0x0c
 void dec_c(); //0x0d
-void ld_c_u8(uint8_t value); //0x0e
+void ld_c_u8(uint8_t operand); //0x0e
 void rrca(); //0x0f
 void stop(); //0x10
-void ld_de_u16(uint16_t value); //0x11
+void ld_de_u16(uint16_t operand); //0x11
 void ld_dep_a(); //0x12
 void inc_de(); //0x13
 void inc_d(); //0x14
 void dec_d(); //0x15
-void ld_d_u8(uint8_t value); //0x16
+void ld_d_u8(uint8_t operand); //0x16
 void rla(); //0x17
-void jr_i8(int8_t value); //0x18 -- unconditional jump to relative address
+void jr_i8(int8_t operand); //0x18 -- unconditional jump to relative address
 void add_hl_de(); //0x19
 void ld_a_dep(); //0x1a
 void dec_de(); //0x1b
 void inc_e(); //0x1c
 void dec_e(); //0x1d
-void ld_e_u8(uint8_t value); //0x1e
+void ld_e_u8(uint8_t operand); //0x1e
 void rra(); //0x1f
-void jr_nz_i8(int8_t value); //0x20 -- conditional jump to relative address if not zero
-void ld_hl_u16(uint16_t value); //0x21
+void jr_nz_i8(int8_t operand); //0x20 -- conditional jump to relative address if not zero
+void ld_hl_u16(uint16_t operand); //0x21
 void ldi_hlp_a(); //0x22 
 void inc_hl(); //0x23
 void inc_h(); //0x24
 void dec_h(); //0x25
-void ld_h_u8(uint8_t value); //0x26
+void ld_h_u8(uint8_t operand); //0x26
 void daa(); //0x27
-void jr_z_i8(int8_t value); //0x28 -- conditional jump to relative address if zero
+void jr_z_i8(int8_t operand); //0x28 -- conditional jump to relative address if zero
 void add_hl_hl(); //0x29
 void ldi_a_hlp(); //0x2a
 void dec_hl(); //0x2b
 void inc_l(); //0x2c
 void dec_l(); //0x2d
-void ld_l_u8(uint8_t value); //0x2e
+void ld_l_u8(uint8_t operand); //0x2e
 void cpl(); //0x2f
-void jr_nc_i8(int8_t value); //0x30
-void ld_sp_u16(uint16_t value); //0x31
+void jr_nc_i8(int8_t operand); //0x30
+void ld_sp_u16(uint16_t operand); //0x31
 void ld_hlp_a(); //0x32
 void inc_sp(); //0x33
 void inc_hlp(); //0x34
 void dec_hlp(); //0x35
-void ld_hlp_u8(uint8_t value); //0x36
+void ld_hlp_u8(uint8_t operand); //0x36
 void scf(); //0x37
-void jr_c_i8(int8_t value); //0x38
+void jr_c_i8(int8_t operand); //0x38
 void add_hl_sp(); //0x39
 void ld_a_hlp(); //0x3a
 void dec_sp(); //0x3b
 void inc_a(); //0x3c
 void dec_a(); //0x3d
-void ld_a_u8(uint8_t value); //0x3e
+void ld_a_u8(uint8_t operand); //0x3e
 void ccf(); //0x3f
 void ld_b(); //0x40
 void ld_b_c(); //0x41
@@ -222,19 +222,69 @@ void cp_a_hlp(); //0xbe
 void cp_a(); //0xbf
 void ret_nz(); //0xc0
 void pop_bc(); //0xc1
-void jp_nz_u16(uint16_t value); //0xc2
-void jp_u16(uint16_t value); //0xc3
-void call_nz_u16(uint16_t value); //0xc4
+void jp_nz_u16(uint16_t operand); //0xc2
+void jp_u16(uint16_t operand); //0xc3
+void call_nz_u16(uint16_t operand); //0xc4
 void push_bc(); //0xc5
-void add_a_u8(uint8_t value); //0xc6
+void add_a_u8(uint8_t operand); //0xc6
 void rst_00(); //0xc7
 void ret_z(); //0xc8
 void ret(); //0xc9
-void jp_z_u16(uint16_t value); //0xca
+void jp_z_u16(uint16_t operand); //0xca
 void prefix_cb(); //0xcb
-void call_z_u16(uint16_t value); //0xcc
-void call_u16(uint16_t value); //0xcd
-void adc_a_u8(uint8_t value); //0xce
+void call_z_u16(uint16_t operand); //0xcc
+void call_u16(uint16_t operand); //0xcd
+void adc_a_u8(uint8_t operand); //0xce
 void rst_08(); //0xcf
+void ret_nc(); //0xd0
+void pop_de(); //0xd1
+void jp_nc_u16(uint16_t operand); //0xd2
+//0xd3 not implemented
+void call_nc_u16(uint16_t operand); //0xd4
+void push_de(); //0xd5
+void sub_a_u8(uint8_t operand); //0xd6
+void rst_10(); //0xd7
+void ret_c(); //0xd8
+void reti(); //0xd9
+void jp_c_u16(uint16_t operand); //0xda
+//0xdb not implemented
+void call_c_u16(uint16_t operand); //0xdc
+//0xdd not implemented
+void sbc_a_u8(uint8_t operand); //0xde
+void rst_18(); //0xdf
+void ld_ff_u8_a(); //0xe0
+void pop_hl(); //0xe1
+void ld_ff_c_a(); //0xe2
+//0xe3 not implemented
+//0xe4 not implemented
+void push_hl(); //0xe5
+void and_a_u8(uint8_t operand); //0xe6
+void rst_20(); //0xe7
+void add_sp_i8(int8_t operand); //0xe8
+void jp_hl(); //0xe9
+void ld_u16p_a(); //0xea
+//0xeb
+//0xec
+//0xed
+void xor_a_u8(uint8_t operand); //0xee
+void rst_28(); //0xef
+void ld_a_ff_u8(uint8_t operand); //0xf0
+void pop_af(); //0xf1
+void ld_a_ff_c(); //0xf2
+void di(); //0xf3
+//0xf4
+void push_af(); //0xf5
+void or_a_u8(uint8_t operand); //0xf6
+void rst_30(); //0xf7
+void ld_hl_sp_i8(int8_t operand); //0xf8
+void ld_sp_hl(); //0xf9
+void ld_a_u16p(uint16_t operand); //0xfa
+void ei(); //0xfb
+//0xfc
+//0xfd
+cp_a_u8(uint8_t operand); //0xfe
+rst_38(); //0xff
+
+//CB instructions todo
 
 #endif
